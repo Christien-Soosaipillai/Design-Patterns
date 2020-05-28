55886f4517ba095f16dce3ac7eed14742f1e685e
Christien-Soosaipillai <christien2011@hotmail.ca>
2020-05-27 23:23:17 -0400

    made a small comment to explain logic for AdapterPatternDemo.java

--- structural-pattern/src/main/java/com/christien/01_adapterpattern/AdapterPattenDemo.java
+++ structural-pattern/src/main/java/com/christien/01_adapterpattern/AdapterPattenDemo.java
@@ -15,9 +15,9 @@ class AdapterPattenDemo{
 		System.out.println("\nWoodenCardinal implementation of ToyBird interface");
 		woodenCardinal.squeak();
 
+		//Here the "ToyBird birdAdapter" calls the method "squek()" however it behaves like a "Bird" becase inside that method it calls "bird.makeSound()"
 		System.out.println("\nToyBird implementation behaving like Bird interface");
 		birdAdapter.squeak();
-
 	}
 
 }
